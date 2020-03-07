8ea1383ad5bf6b26c43873a43f1f999eb6207dc6
Noctua <mggrandmdg@gmail.com>
2020-03-06 18:01:59 -0800

    Update matrix.js

--- matrix.js
+++ matrix.js
@@ -1,9 +1,12 @@
 
 /* Usage
-*  let matrix1 = new Matrix(3, 2)
+*  let matrix1 = new Matrix(3, 2);
 *  let matrix2 = new Matrix(2, 4);
 *  matrix1.mult(matrix2);
-*  
+*  let matrix3 = Matrix.mult(matrix1, matrix2); //this line is using the static mult function
+*
+*      note that the actually matrixdata is found using .matrix
+*      e.g. matrix1.matrix holds the underlying matrix data
 *  
 * this is deliberately made innefficient in order to aid understanding
 * a significant optimisation would be to not use nested arrays however
@@ -36,7 +39,7 @@ class Matrix {
         for(let row = 0; row < result.rows; row++) {    // resulting matrix will have dimensions of the 1st matrix's rows and the # of columns in the 2nd matrix
           for(let col = 0; col < result.cols; col++) {
             let sum = 0;
-            for(let index = 0; index < result.rows; index++) {
+            for(let index = 0; index < this.cols; index++) {
               sum += this.matrix[row][index] * n.matrix[index][col];
             }
             result.matrix[row][col] = sum;
@@ -51,6 +54,7 @@ class Matrix {
         }
       }
       this.print();
+      return this;
     }
   
     
@@ -102,6 +106,20 @@ class Matrix {
           result.matrix[col][row] = this.matrix[row][col];
         }
       }
+      return result;
+    }
+  
+    static mult(m1, m2) {
+        const result = new Matrix(m1.rows, m2.cols);
+        for(let row = 0; row < result.rows; row++) {    // resulting matrix will have dimensions of the 1st matrix's rows and the # of columns in the 2nd matrix
+          for(let col = 0; col < result.cols; col++) {
+            let sum = 0;
+            for(let index = 0; index < m1.cols; index++) {
+              sum += m1.matrix[row][index] * m2.matrix[index][col];
+            }
+            result.matrix[row][col] = sum;
+          }
+        }
       return result;
     }
     
