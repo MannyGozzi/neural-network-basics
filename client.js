fdabcecc9a263f5fd15e3cbf81c09de328ea6074
Noctua <mggrandmdg@gmail.com>
2020-03-06 18:10:28 -0800

    Update client.js

--- client.js
+++ client.js
@@ -1,11 +1,12 @@
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    let m1 = new Matrix(2, 3);
-    m1.randomize(); 
-    let m2 = new Matrix(3, 2);
-    m2.randomize();
-    console.table(m1.mult(m2).matrix);
+    let m1 = new Matrix(2, 3);  m1.randomize(); 
+    let m2 = new Matrix(3, 2);  m2.randomize();
+  
+    function doubleIt(value) {return value * 2};
+    m1.map(doubleIt);
+    m1.print();
   }
   
   
